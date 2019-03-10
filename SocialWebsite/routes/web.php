414953c05b81d393384d50b30323233a74988f03
Youssef <yussefh.galal@gmail.com>
2019-03-11 01:50:12 +0200

    fixing conflicts with nabil, again

--- SocialWebsite/routes/web.php
+++ SocialWebsite/routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-<<<<<<< HEAD
-Route::get('/home', 'HomeController@ReadPost')->name('home');
-Route::get('/CreatePost','HomeController@CreatePost');
-Route::Post('/CreatePost','HomeController@CreatePost');
+//<<<<<<< HEAD
+// Route::get('/home', 'HomeController@ReadPost')->name('home');
+// Route::get('/CreatePost','HomeController@CreatePost');
+// Route::Post('/CreatePost','HomeController@CreatePost');
 
 // Routes for PageController
 //Route::resources('pages', 'Page\PageController');
@@ -31,11 +31,11 @@ Route::get('pages/{page}', 'Page\PageController@show');
 Route::get('pages/{page}/edit', 'Page\PageController@edit');
 Route::patch('pages/{page}', 'Page\PageController@update');
 Route::delete('pages/{page}', 'Page\PageController@destroy');
-=======
+//=======
 Route::get('/home', 'HomePostController@index')->name('home');
 Route::get('/CreatePost','HomePostController@create')->name('home-post.create');
 
 Route::post('/CreatePost','HomePostController@store')->name('home-post.store');
 
 Route::post('/home','HomeLikeController@store')->name('like');
->>>>>>> 88925d1b87c6c96fa0fb338fbb1d0cbe9e3a7972
+//>>>>>>> 88925d1b87c6c96fa0fb338fbb1d0cbe9e3a7972
